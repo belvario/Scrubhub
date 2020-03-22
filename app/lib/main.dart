c443d2ec7d3f729783301777d71651393903cdc0
mkelandis <landis.michael@gmail.com>
2020-03-22 14:38:44 -0400

    Added Hello World Text

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Hello World. You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
