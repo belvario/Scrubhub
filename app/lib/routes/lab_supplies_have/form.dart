8ff085784d21c735b2e7db0a85265be77093ed60
mkelandis <landis.michael@gmail.com>
2020-03-27 08:31:59 -0400

    missing file from last commit

--- app/lib/routes/lab_supplies_have/form.dart
+++ app/lib/routes/lab_supplies_have/form.dart
@@ -1,7 +1,6 @@
 import 'package:app/form_fields/medical_equipment.dart';
 import 'package:app/form_fields/other_option.dart';
 import 'package:app/form_fields/states.dart';
-import 'package:app/widgets/viz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
